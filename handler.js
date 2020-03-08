54075ba7fee7d2662a160acb44ff50fe9c268834
Ubuntu <ubuntu@ip-172-31-89-39.ec2.internal>
2020-03-08 21:39:48 +0000

    update: fixed text output error

--- handler.js
+++ handler.js
@@ -1,5 +1,5 @@
 exports.handler = (event, context, callback) => {
     var body = event.Records[0].messageAttributes;
-    console.log("text-body: ", body);
+    console.log("text: ", body.message);
     console.log(body);
 };
